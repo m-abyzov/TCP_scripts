8db69d3aa6d0a2d8f434ffaf9f02716e57822829
Mikhail Abyzov <mikhaijl7@gmail.com>
2020-05-02 12:39:41 -0700

    added dir creation if not exists

--- getlo_processing/getlo_numeric_results_processor.py
+++ getlo_processing/getlo_numeric_results_processor.py
@@ -1,12 +1,17 @@
-PROJECT_ID = "chart"
+from pathlib import Path
+
+PROJECT_ID = "lang"
+PROJECT_DIR = f"../D4J/{PROJECT_ID}/method_coverage/getlo/2"
 
 if __name__ == "__main__":
-    with open(f"../D4J/{PROJECT_ID}/getlo/{PROJECT_ID}_numeric_ordering", "r") as space_file:
+    with open(f"{PROJECT_DIR}/{PROJECT_ID}_numeric_ordering", "r") as space_file:
         test_ids = []
         for line in space_file:
             if line.strip() == "" or line.split()[0].isalpha():
                 continue
             test_ids.extend(line.split())
 
-    with open(f"../D4J/{PROJECT_ID}/getlo/{PROJECT_ID}_processed_numeric_ordering", "w") as proc_space_file:
+    Path(PROJECT_DIR).mkdir(parents=True, exist_ok=True)
+
+    with open(f"{PROJECT_DIR}/{PROJECT_ID}_processed_numeric_ordering", "w") as proc_space_file:
         proc_space_file.write(",".join(test_ids))
