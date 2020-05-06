ee566b833f1897139d289170d322b426a3478878
Mikhail Abyzov <mikhaijl7@gmail.com>
2020-05-06 10:19:26 -0700

    edited Kanonizo file processing in order to process bunch of files of different projects.

--- kanonizo_processing/kanonizo_res_file_processor.py
+++ kanonizo_processing/kanonizo_res_file_processor.py
@@ -1,28 +1,53 @@
 from pathlib import Path
+import os
 
-PROJECT_ID = "chart"
+PROJECT_IDs = ["chart"]
+D4J_ROOT_DIR = "../D4J"
 
-KANONIZO_RES_FILE = f"../D4J/chart/kanonizo/chart_random_res_2.csv"
-PROCESSED_KANONIZO_RES_FILE = KANONIZO_RES_FILE.replace(KANONIZO_RES_FILE.split("/")[-1], "") \
-                              + "processed/processed_" + KANONIZO_RES_FILE.split("/")[-1]
 
-Path("/".join(PROCESSED_KANONIZO_RES_FILE.split('/')[:-1])).mkdir(parents=True, exist_ok=True)
+def construct_filename(raw_file_path):
+    """
+    Creates dir for the processed files, and returns filename of the
+    file, into which processed test names will be written.
+    :param raw_file_path: filepath to the raw (as it produced by the
+    tool) Kanonizo results.
+    :return: filename of the file, into which processed test names
+    will be written.
+    """
+    # name of the algo inside kanonizo. It should be in the filename
+    # after first '_' symbol.
+    algo_name = raw_file_path.split('/')[-1].split('_')[1]
+    processed_file_path = '/'.join(
+        raw_file_path.split('/')[:-2] + ['processed'] + [algo_name]
+    )
+    # creating a folder 'processed' in the D4J project dir.
+    Path(processed_file_path).mkdir(parents=True, exist_ok=True)
 
-res_string = ""
+    # full processed filepath with filename.
+    return (processed_file_path
+            + f"/processed_{raw_file_path.split('/')[-1]}")
 
-with open(KANONIZO_RES_FILE, "r") as file_to_process:
-    for line_num, line in enumerate(file_to_process):
-        if line_num == 0:
-            continue
 
-        processed_line = line.split(',')[0]
-        method_name = processed_line.split('(')[0]
-        # if len(processed_line.split("(")) == 1:
-        #     print()
+if __name__ == "__main__":
+    for PROJECT_ID in PROJECT_IDs:
+        KANONIZO_RAW_FILES_DIR = f"{D4J_ROOT_DIR}/{PROJECT_ID}/kanonizo/raw"
+        for file in os.scandir(KANONIZO_RAW_FILES_DIR):
+            raw_file_path = file.path
 
-        full_name = f'{processed_line.split("(")[1][:-1]}::{method_name}\n'
+            # read the raw file
+            res_string = ""
+            with open(raw_file_path, "r") as file_to_process:
+                for line_num, line in enumerate(file_to_process):
+                    if line_num == 0:
+                        continue
 
-        res_string += full_name
+                    processed_line = line.split(',')[0]
+                    method_name = processed_line.split('(')[0]
 
-with open(PROCESSED_KANONIZO_RES_FILE, "w") as processed_file:
-    processed_file.write(res_string[:-1])
+                    full_name = f'{processed_line.split("(")[1][:-1]}::{method_name}\n'
+                    res_string += full_name
+
+            # write to the processed file.
+            processed_file_path = construct_filename(file.path)
+            with open(processed_file_path, "w") as processed_file:
+                processed_file.write(res_string[:-1])
---
+++ kanonizo_processing/old_kanonizo_res_file_processor.py
@@ -0,0 +1,27 @@
+"""
+This code is for processing of the old version of the Kanonizo tool.
+Uncomment & check it if needed.
+"""
+
+# KANONIZO_RES_FILE = "../D4J/time/kanonizo/joda_randomsearch_res.csv"
+# PROCESSED_KANONIZO_RES_FILE = KANONIZO_RES_FILE.replace(KANONIZO_RES_FILE.split("/")[-1], "") \
+#                               + "processed/processed_" + KANONIZO_RES_FILE.split("/")[-1]
+#
+# res_string = ""
+#
+# with open(KANONIZO_RES_FILE, "r") as file_to_process:
+#     for line_num, line in enumerate(file_to_process):
+#         if line_num == 0:
+#             continue
+#
+#         processed_line = line.split(',')[0]
+#
+#         method_name = processed_line.split('.')[-1]
+#         class_name = processed_line.replace(method_name, "")[:-1]
+#
+#         full_name = f'{class_name}::{method_name}\n'
+#
+#         res_string += full_name
+#
+# with open(PROCESSED_KANONIZO_RES_FILE, "w") as processed_file:
+#     processed_file.write(res_string[:-1])
