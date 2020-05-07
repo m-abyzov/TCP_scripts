e777b7a725d4bc29d32dcd454328a9b3f7bd164f
Mikhail Abyzov <mikhaijl7@gmail.com>
2020-05-07 11:07:41 -0700

    modified GeTLO results processing scripts

--- getlo_processing/getlo_numeric_results_processor.py
+++ getlo_processing/getlo_numeric_results_processor.py
@@ -1,17 +1,14 @@
-from pathlib import Path
 
-PROJECT_ID = "lang"
-PROJECT_DIR = f"../D4J/{PROJECT_ID}/method_coverage/getlo/2"
+def process_5_numeric_orderings_to_ids(PROJECT_ID, PROJECT_DIR, n=5):
+    for i in range(1, n + 1):
+        full_project_dir = f"{PROJECT_DIR}/{i}"
 
-if __name__ == "__main__":
-    with open(f"{PROJECT_DIR}/{PROJECT_ID}_numeric_ordering", "r") as space_file:
-        test_ids = []
-        for line in space_file:
-            if line.strip() == "" or line.split()[0].isalpha():
-                continue
-            test_ids.extend(line.split())
+        with open(f"{full_project_dir}/{PROJECT_ID}_numeric_ordering", "r") as space_file:
+            test_ids = []
+            for line in space_file:
+                if line.strip() == "" or line.split()[0].isalpha():
+                    continue
+                test_ids.extend(line.split())
 
-    Path(PROJECT_DIR).mkdir(parents=True, exist_ok=True)
-
-    with open(f"{PROJECT_DIR}/{PROJECT_ID}_processed_numeric_ordering", "w") as proc_space_file:
-        proc_space_file.write(",".join(test_ids))
+        with open(f"{full_project_dir}/{PROJECT_ID}_processed_numeric_ordering", "w") as proc_space_file:
+            proc_space_file.write(",".join(test_ids))
---
+++ getlo_processing/main.py
@@ -0,0 +1,25 @@
+from getlo_numeric_results_processor import process_5_numeric_orderings_to_ids
+from getlo_name_id_mapper import process_5_ids_orderings_to_test_names
+from apfd import estimate_single_experiment
+
+
+# THis is a Script for calculating APFD for the GeTLO in the chosen
+# D4J project.
+PROJECT_IDs = ["closure", "lang", "chart", "time", "math"]
+# PROJECT_IDS = [PROJECT_ID]
+
+if __name__ == "__main__":
+    n = 5
+    for PROJECT_ID in PROJECT_IDs:
+        PROJECT_DIR = f"../D4J/{PROJECT_ID}/getlo"
+        process_5_numeric_orderings_to_ids(PROJECT_ID, PROJECT_DIR, n)
+        process_5_ids_orderings_to_test_names(PROJECT_ID, PROJECT_DIR, n)
+
+        APFD_results = []
+        BUGS_INFO_PATH = f"../D4J/D4J_bug_extractor/{PROJECT_ID}_bugs_revealing_info.csv"
+        for i in range(1, n + 1):
+            TEST_ORDER_PATH = f"{PROJECT_DIR}/{i}/{PROJECT_ID}_tests_ordering"
+            APFD_results.append(estimate_single_experiment(TEST_ORDER_PATH, BUGS_INFO_PATH, False))
+
+        with open(f'{PROJECT_DIR}/{PROJECT_ID}_GeTLO_{n}_APFDs', 'w') as res_file:
+            res_file.write(f"{PROJECT_ID}_GeTLO_{n}_APFDs: {str(APFD_results)}")
