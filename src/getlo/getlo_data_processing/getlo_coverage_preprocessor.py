9f2819c2a66a0049e6edd03425a4ce7fab5613ed
Mikhail Abyzov <mikhaijl7@gmail.com>
2020-06-25 11:41:36 -0700

    added feature of choosing projects before launching from the bash script.

--- src/getlo/getlo_data_processing/getlo_coverage_preprocessor.py
+++ src/getlo/getlo_data_processing/getlo_coverage_preprocessor.py
@@ -2,16 +2,14 @@ import pandas as pd
 import numpy as np
 from sys import argv as args
 
-projects = ["chart", "lang", "math", "closure", "time"]
-
 
 if __name__ == "__main__":
+    getlo_path = args[1]  # path till unpacked GeTLO project, e.g. "/home/misha/TCP_tools/GeTLO"
+    projects = args[2:]
     for project_id in projects:
         coverage_matrix_path = f'D4J_projects/{project_id}/{project_id}_method_coverage_matrix.txt'
         coverage_df = pd.read_csv(coverage_matrix_path, sep=" ", header=None)
         coverage_df = coverage_df.iloc[:, :-1]  # remove last column
         coverage_df = coverage_df[:-1]  # remove last row
-
-        getlo_path = args[1]  # path till unpacked GeTLO project, e.g. "/home/misha/TCP_tools/GeTLO"
         processed_matrix_path = f'{getlo_path}/{project_id}_getlo_method_coverage_matrix.txt'
         np.savetxt(processed_matrix_path, coverage_df.T.values, fmt='%d')
