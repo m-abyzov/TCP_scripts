abed9967714a057f221401f729994c1cddcc2e06
Mikhail Abyzov <mikhaijl7@gmail.com>
2020-06-26 17:09:55 -0700

    made scripts for movement of modificare results to the eval dir

--- execution_scripts/run_TCP_tools.sh
+++ execution_scripts/run_TCP_tools.sh
@@ -1,6 +1,7 @@
 # provide 3 args (dirs actual for your machine):
 # 1) Path till the cloned GeTLO project, e.g. "/home/misha/TCP_tools/GeTLO"
 # 2) Path till the cloned Modificare project, e.g. "/home/misha/TCP_tools/modificare"
+# 3) Path till the current project, e.g. "/home/misha/PycharmProjects/TCP_scripts"
 # TODO: Uncomment for all the projects!
 #projects=(chart lang math closure time)
 projects=(chart lang)
@@ -11,3 +12,4 @@ python ./src/getlo/getlo_data_processing/getlo_coverage_preprocessor.py $1 ${pro
 # Copy data and run the Modificare tool.
 python ./src/modificare/copy_data.py $2 ${projects[*]}
 Rscript $2/run_experiments.R ${projects[*]}
+python ./src/modificare/move_results.py $2 $3 ${projects[*]}
--- src/modificare/move_results.py
+++ src/modificare/move_results.py
@@ -1,3 +1,5 @@
+from sys import argv as args
+from os import scandir
 import subprocess
 
 BASE_DIR = "D4J_projects"
@@ -9,3 +11,13 @@ def run_from_terminal(query):
                      stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
 if __name__ == "__main__":
+    MODIFICARE_PATH = f"{args[1]}/results"
+    evaluation_path = f"{args[2]}/D4J_projects"
+    projects = args[3:]
+    for project in projects:
+        MODIFICARE_PATH += f"/{project}"
+        evaluation_path += f"/{project}/results/prioritized_orderings/modificare"
+        for algorithm in MODIFICARE_ALGORITHMS:
+            for file in scandir(f"{MODIFICARE_PATH}/{algorithm}"):
+                query = f"mv {file.path} {evaluation_path}/{algorithm}"
+                run_from_terminal(query)
