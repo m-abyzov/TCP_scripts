14e8c60b4f7719391a0dadc3c0ee14af9ceda50d
mishabuzov <mikhaijl@gmail.com>
2020-06-28 01:58:39 +0300

    changed dir of the GeTLO project

--- src/getlo/getlo_data_processing/getlo_coverage_preprocessor.py
+++ src/getlo/getlo_data_processing/getlo_coverage_preprocessor.py
@@ -4,12 +4,11 @@ from sys import argv as args
 
 
 if __name__ == "__main__":
-    getlo_path = args[1]  # path till unpacked GeTLO project, e.g. "/home/misha/TCP_tools/GeTLO"
-    projects = args[2:]
+    projects = args[1:]
     for project_id in projects:
         coverage_matrix_path = f'D4J_projects/{project_id}/{project_id}_method_coverage_matrix.txt'
         coverage_df = pd.read_csv(coverage_matrix_path, sep=" ", header=None)
         coverage_df = coverage_df.iloc[:, :-1]  # remove last column
         coverage_df = coverage_df[:-1]  # remove last row
-        processed_matrix_path = f'{getlo_path}/{project_id}_getlo_method_coverage_matrix.txt'
+        processed_matrix_path = f'tools/getlo/{project_id}_getlo_method_coverage_matrix.txt'
         np.savetxt(processed_matrix_path, coverage_df.T.values, fmt='%d')
