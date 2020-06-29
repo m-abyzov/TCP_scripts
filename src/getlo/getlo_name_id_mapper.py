c4fd11142e0b3a96f21e6ff62115000f4c6ce0c9
Mikhail Abyzov <mikhaijl7@gmail.com>
2020-06-29 13:51:03 -0700

    Rewritten a function for GeTLO results postprocessing

--- src/getlo/getlo_name_id_mapper.py
+++ src/getlo/getlo_name_id_mapper.py
@@ -1,8 +1,13 @@
+from pathlib import Path
+from sys import argv as args
+from os import scandir
 
-def process_5_ids_orderings_to_test_names(PROJECT_ID, PROJECT_DIR, n=5):
+BASE_DIR = "D4J_projects"
+
+
+def extract_test_names(PROJECT_ID):
     # processed with XMLCoverageParser (CoberturaCoverageParser) all test file .
-    ALL_TEST_FILE = F"../D4J/{PROJECT_ID}/{PROJECT_ID}_id_name_mapping.txt"
-
+    ALL_TEST_FILE = F"{BASE_DIR}/{PROJECT_ID}/{PROJECT_ID}_id_name_mapping.txt"
     test_id_name_mapper = {}
 
     with open(ALL_TEST_FILE, "r") as all_tests_file:
@@ -10,20 +15,27 @@ def process_5_ids_orderings_to_test_names(PROJECT_ID, PROJECT_DIR, n=5):
             test_name = test_name.strip().split()[0]
             if len(test_name) > 0:
                 test_id_name_mapper[test_id + 1] = test_name
+    return test_id_name_mapper
 
-    for i in range(1, n + 1):
-        full_project_dir = f"{PROJECT_DIR}/{i}"
-        # processed numeric ordering with "getlo_numeric_results_processor.py".
-        NUMERIC_ORDERING_FILE = F"{full_project_dir}/{PROJECT_ID}_processed_numeric_ordering"
-        RESULT_ORDERING_FILE = F"{full_project_dir}/{PROJECT_ID}_tests_ordering"
 
-        with open(NUMERIC_ORDERING_FILE, "r") as ordering_file, \
-                open(RESULT_ORDERING_FILE, "w") as result_file:
-            tests_id_order = [int(test_id) for test_id in ordering_file.read().split(',') if test_id != ""]
+if __name__ == "__main__":
+    projects = args[1:]
+    for project_id in projects:
+        test_id_name_mapper = extract_test_names(project_id)
+        raw_getlo_dir = f"{BASE_DIR}/{project_id}/results/prioritized_orderings/getlo/raw"
+        processed_getlo_dir = f"{BASE_DIR}/{project_id}/results/prioritized_orderings/getlo/processed"
+        Path(processed_getlo_dir).mkdir(parents=True, exist_ok=True)
 
-            for i in range(0, len(tests_id_order)):
-                test_id = tests_id_order[i]
-                if test_id in test_id_name_mapper:
-                    result_file.write(test_id_name_mapper[test_id])
-                    if i < len(tests_id_order) - 1:
-                        result_file.write("\n")
+        for numeric_file in scandir(raw_getlo_dir):
+            RESULT_ORDERING_FILE = F"{processed_getlo_dir}/{project_id}_tests_ordering"
+
+            with open(numeric_file.path, "r") as ordering_file, \
+                    open(RESULT_ORDERING_FILE, "w") as result_file:
+                tests_id_order = [int(test_id) for test_id in ordering_file.read().split(',') if test_id != ""]
+
+                for i in range(0, len(tests_id_order)):
+                    test_id = tests_id_order[i]
+                    if test_id in test_id_name_mapper:
+                        result_file.write(test_id_name_mapper[test_id])
+                        if i < len(tests_id_order) - 1:
+                            result_file.write("\n")
