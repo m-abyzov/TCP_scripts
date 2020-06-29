96a90c00099d169df9c0a2e711a9352cc64cacde
Mikhail Abyzov <mikhaijl7@gmail.com>
2020-06-29 13:49:34 -0700

    Moved redundant GeTLO scripts to the separate dir, added dir creation during preprocessing

--- src/getlo/__init__.py
+++
@@ -1,2 +0,0 @@
-from getlo_processing.getlo_numeric_results_processor import process_5_numeric_orderings_to_ids
-from getlo_processing.getlo_name_id_mapper import process_5_ids_orderings_to_test_names
--- src/getlo/getlo_coverage_preprocessor.py
+++ src/getlo/getlo_coverage_preprocessor.py
@@ -1,14 +1,20 @@
+from pathlib import Path
+
 import pandas as pd
 import numpy as np
 from sys import argv as args
 
+BASE_DIR = f'D4J_projects'
 
 if __name__ == "__main__":
     projects = args[1:]
     for project_id in projects:
-        coverage_matrix_path = f'D4J_projects/{project_id}/{project_id}_method_coverage_matrix.txt'
+        coverage_matrix_path = f'{BASE_DIR}/{project_id}/{project_id}_method_coverage_matrix.txt'
         coverage_df = pd.read_csv(coverage_matrix_path, sep=" ", header=None)
         coverage_df = coverage_df.iloc[:, :-1]  # remove last column
         coverage_df = coverage_df[:-1]  # remove last row
         processed_matrix_path = f'tools/getlo/{project_id}_getlo_method_coverage_matrix.txt'
         np.savetxt(processed_matrix_path, coverage_df.T.values, fmt='%d')
+
+        GETLO_RAW_FILES_DIR = f'{BASE_DIR}/{project_id}/results/prioritized_orderings/getlo/raw'
+        Path(GETLO_RAW_FILES_DIR).mkdir(parents=True, exist_ok=True)
